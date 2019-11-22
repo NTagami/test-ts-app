5a31a8c821cd537780e4c3062e341514c48ea594
Naoki Tagami <Naoki.Tagami@sony.com>
2019-11-22 09:48:54 +0900

    test

--- src/action.ts
+++ src/action.ts
@@ -8,12 +8,6 @@ export const addTodo = createAction("ADD_TODO", (todo: string) => ({
     title: todo
   }
 }))();
-/*
-export enum CommonActionType {
-  DUMMY = "DUMMY",
-  ERROR = "ERROR",
-  CLEAR_ERROR = "CLEAR_ERROR"
-}*/
 
 export const dummyAction = createAction("DUMMY", (message: string) => ({
   message
@@ -22,42 +16,9 @@ export const errorMessage = createAction("ERROR", (message: string) => ({
   message
 }))();
 export const clearError = createAction("CLEAR_ERROR")();
-/*
-export interface Dummy extends Action {
-  type: CommonActionType.DUMMY;
-  payload: {
-    message: string;
-  };
-}
 
-export interface ErrorMessage extends Action {
-  type: CommonActionType.ERROR;
-  payload: {
-    message: string;
-  };
-}
-
-export interface ClearErrorMessage extends Action {
-  type: CommonActionType.CLEAR_ERROR;
-}
-*/
 export type TodoAction = ActionType<typeof addTodo>;
 export type CommonAction =
   | ActionType<typeof dummyAction>
   | ActionType<typeof errorMessage>
   | ActionType<typeof clearError>;
-/*
-export function dummyAction(message: string): Dummy {
-  return {
-    payload: { message },
-    type: CommonActionType.DUMMY
-  };
-}
-
-export function errorMessage(message: string): ErrorMessage {
-  return {
-    payload: { message },
-    type: CommonActionType.ERROR
-  };
-}
-*/
